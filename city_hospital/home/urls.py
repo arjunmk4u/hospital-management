0489a7a9713dab2b1aeed0c92757f3dceefd175f
arjunmk4u <arjunmk4u@gmail.com>
2024-04-11 14:00:29 +0530

    Added static style and created base html

--- city_hospital/home/urls.py
+++ city_hospital/home/urls.py
@@ -2,10 +2,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('about/', views.about),
-    path('booking/', views.Booking),
-    path('doctors/', views.Doctors),
-    path('contact/', views.Contact),
-    path('department/', views.departmnet),
+    path('', views.index, name='home'),
+    path('about/', views.about, name='about'),
+    path('booking/', views.Booking, name='booking'),
+    path('doctors/', views.Doctors, name='doctors'),
+    path('contact/', views.Contact, name='contact'),
+    path('department/', views.departmnet, name='dept'),
 ]
